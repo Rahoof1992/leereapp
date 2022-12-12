0160655141ff73ff2ddd84ad730e29193bcb54db
rahoofjamil <rahoof_1@live.de>
2022-12-12 15:45:13 +0100

    Initial Commit

---
+++ leereapp/leereappApp.swift
@@ -0,0 +1,17 @@
+//
+//  leereappApp.swift
+//  leereapp
+//
+//  Created by Rahoof Jamil on 12.12.22.
+//
+
+import SwiftUI
+
+@main
+struct leereappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
